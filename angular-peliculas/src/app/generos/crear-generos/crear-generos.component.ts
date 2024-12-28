567823760b07ed2756b56ecfdb6af65d2e684e6a
Guillermo Flores <gflores.93@hotmail.com>
2024-12-28 17:30:24 -0600

    feat: add reactive form for create and edit genre

--- angular-peliculas/src/app/generos/crear-generos/crear-generos.component.ts
+++ angular-peliculas/src/app/generos/crear-generos/crear-generos.component.ts
@@ -1,20 +1,27 @@
 import { Component, inject } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
-import { Router } from '@angular/router';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+
+import { Router, RouterLink } from '@angular/router';
+import { GeneroCreacionDTO } from '../generos';
+import { FormularioGeneroComponent } from "../formulario-genero/formulario-genero.component";
 
 @Component({
   selector: 'app-crear-generos',
-  imports: [MatButtonModule],
+  imports: [RouterLink, FormularioGeneroComponent],
   templateUrl: './crear-generos.component.html',
   styleUrl: './crear-generos.component.css'
 })
 export class CrearGenerosComponent {
 
-  router = inject(Router);
+  private router = inject(Router);
 
-  guardarCambios() {
+  guardarCambios(genero: GeneroCreacionDTO) {
     // TODO
-    this.router.navigate(['/generos']);
+    // this.router.navigate(['/generos']);
+    console.log('creando el genero', genero);
   }
 
 }
