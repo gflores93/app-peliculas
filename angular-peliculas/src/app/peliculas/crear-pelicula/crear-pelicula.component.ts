641ab976ce799e0ee8de18459bf19c504908e726
Guillermo Flores <gflores.93@hotmail.com>
2024-12-30 17:56:24 -0600

    feat: add create and edit peliculas form

--- angular-peliculas/src/app/peliculas/crear-pelicula/crear-pelicula.component.ts
+++ angular-peliculas/src/app/peliculas/crear-pelicula/crear-pelicula.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { PeliculaCreacionDTO } from '../peliculas';
+import { FormularioPeliculasComponent } from "../formulario-peliculas/formulario-peliculas.component";
 
 @Component({
   selector: 'app-crear-pelicula',
-  imports: [],
+  imports: [FormularioPeliculasComponent],
   templateUrl: './crear-pelicula.component.html',
   styleUrl: './crear-pelicula.component.css'
 })
 export class CrearPeliculaComponent {
 
+  guardarCambios(pelicula: PeliculaCreacionDTO) {
+    console.log('creando película', pelicula);
+  }
 }
--- angular-peliculas/src/app/peliculas/editar-pelicula/editar-pelicula.component.ts
+++ angular-peliculas/src/app/peliculas/editar-pelicula/editar-pelicula.component.ts
@@ -1,11 +1,26 @@
 import { Component, Input, numberAttribute } from '@angular/core';
+import { PeliculaCreacionDTO, PeliculaDTO } from '../peliculas';
+import { FormularioPeliculasComponent } from "../formulario-peliculas/formulario-peliculas.component";
 
 @Component({
   selector: 'app-editar-pelicula',
-  imports: [],
+  imports: [FormularioPeliculasComponent],
   templateUrl: './editar-pelicula.component.html',
   styleUrl: './editar-pelicula.component.css'
 })
 export class EditarPeliculaComponent {
-  @Input({transform: numberAttribute}) id!: number;
+  @Input({ transform: numberAttribute }) id!: number;
+
+  pelicula: PeliculaDTO =
+    {
+      id: 1,
+      titulo: 'Spider-Man',
+      fechaLanzamiento: new Date(2020, 12, 1),
+      trailer: 'ABC',
+      poster: 'https://upload.wikimedia.org/wikipedia/en/6/6c/Spider-Man_%282002_film%29_poster.jpg?20171215231428'
+    };
+
+  guardarCambios(pelicula: PeliculaCreacionDTO) {
+    console.log('editando película', pelicula);
+  }
 }
