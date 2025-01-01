751baec500a307a2de5807e2125730ef5d394344
Guillermo Flores <gflores.93@hotmail.com>
2024-12-31 19:00:06 -0600

    chore: ASP .NET examples included
    - Test repository created
    - Controller (routing, model binding and OutputCache)
    - Model and attribute validations (included user defined)

---
+++ PeliculasAPI/PeliculasAPI/Controllers/GenerosController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.OutputCaching;
+using PeliculasAPI.Entidades;
+
+namespace PeliculasAPI.Controllers
+{
+    [Route("api/generos")]
+    [ApiController]
+    public class GenerosController : ControllerBase
+    {
+        [HttpGet]
+        [OutputCache]
+        public ActionResult<List<Genero>> Get()
+        {
+            var repositorio = new RepositorioEnMemoria();
+            var generos = repositorio.ObtenerTodosLosGeneros();
+
+            if (generos is null)
+            {
+                return NotFound();
+            }
+            return generos;
+        }
+
+        [HttpGet("{id}")]
+        [OutputCache]
+        public async Task<ActionResult<Genero>> Get(int id)
+        {
+            var repositorio = new RepositorioEnMemoria();
+            var genero = await repositorio.ObtenerPorId(id);
+
+            if (genero is null)
+            {
+                return NotFound();
+            }
+            return genero;
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] Genero genero)
+        {
+            var repositorio = new RepositorioEnMemoria();
+            var yaExisteUnGeneroConDichoNombre = repositorio.Existe(genero.Nombre);
+
+            if (yaExisteUnGeneroConDichoNombre)
+            {
+                return BadRequest($"Ya existe un genero con el nombre {genero.Nombre}");
+            }
+
+            return Ok();
+
+        }
+
+        [HttpPut]
+        public void Put() { }
+
+        [HttpDelete]
+        public void Delete() { }
+
+    }
+}
---
+++ PeliculasAPI/PeliculasAPI/Entidades/Genero.cs
@@ -0,0 +1,15 @@
+﻿using PeliculasAPI.Validaciones;
+using System.ComponentModel.DataAnnotations;
+
+namespace PeliculasAPI.Entidades
+{
+    public class Genero
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "el campo {0} es requerido")]
+        [StringLength(50, ErrorMessage = "el campo {0} debe tener {1} caracteres o menos" )]
+        [PrimeraLetraMayusculaAttribute]
+        public required string Nombre { get; set; }
+
+    }
+}
