3d8bc671e0a6905838aab7bc6eca1bb6682d7e11
Guillermo Flores <gflores.93@hotmail.com>
2025-01-05 12:32:29 -0600

    feat: add Actor in ApplicationDbContext
    - Actor model and DTOs created
    - Migration added

---
+++ PeliculasAPI/PeliculasAPI/DTOs/ActorDTO.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PeliculasAPI.Validaciones;
+using System.ComponentModel.DataAnnotations;
+
+namespace PeliculasAPI.DTOs
+{
+    public class ActorDTO
+    {
+        public int Id { get; set; }
+        public required string Nombre { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+        public string? Foto { get; set; }
+    }
+}
