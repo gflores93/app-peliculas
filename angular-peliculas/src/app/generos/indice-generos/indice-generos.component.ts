20b7fc9009fcce84a34a38d316d94a1ba2d8ebd4
Guillermo Flores <gflores.93@hotmail.com>
2025-01-05 23:48:42 -0600

    refactor: indice-genero and editar-genero

--- angular-peliculas/src/app/generos/indice-generos/indice-generos.component.ts
+++ angular-peliculas/src/app/generos/indice-generos/indice-generos.component.ts
@@ -1,14 +1,13 @@
-import { Component, inject, ViewChild } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { RouterLink } from '@angular/router';
 import { GenerosService } from '../generos.service';
-import { environment } from '../../../environments/environment';
 import { GeneroDTO } from '../generos';
 import { ListadoGenericoComponent } from "../../compartidos/componentes/listado-generico/listado-generico.component";
 import { MatTableModule } from '@angular/material/table';
 import { HttpResponse } from '@angular/common/http';
 import { PaginacionDTO } from '../../compartidos/modelos/PaginacionDTO';
-import { MatPaginator, MatPaginatorModule, PageEvent } from '@angular/material/paginator';
+import { MatPaginatorModule, PageEvent } from '@angular/material/paginator';
 import { SweetAlert2Module } from '@sweetalert2/ngx-sweetalert2';
 
 @Component({
@@ -48,11 +47,11 @@ export class IndiceGenerosComponent {
   borrarRegistro(id: number) {
     this.generosService.borrar(id).subscribe({
       next: () => {
-        this.paginacion = { pagina: 1, recordsPorPagina: 5 };
+        this.paginacion.pagina = 1;
         this.cargarRegistros();
       },
       error: error => console.log(error)
-    })
+    });
   }
 
 }
