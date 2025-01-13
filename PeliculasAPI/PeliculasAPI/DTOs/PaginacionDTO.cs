a57f9f6c35bb266cee4e8a461ed49e320a9d3ffd
Guillermo Flores <gflores.93@hotmail.com>
2025-01-12 23:28:17 -0600

    feat: update pagination DTO to handle negative values

--- PeliculasAPI/PeliculasAPI/DTOs/PaginacionDTO.cs
+++ PeliculasAPI/PeliculasAPI/DTOs/PaginacionDTO.cs
@@ -2,9 +2,18 @@
 {
     public class PaginacionDTO
     {
-        public int Pagina { get; set; } = 1;
+        private int pagina = 1;
         private int recordsPorPagina = 10;
         private readonly int cantidadMaximaRecordsPorPagina = 50;
+        public int Pagina
+        {
+            get { return pagina; }
+            set
+            {
+                // Min value: 0
+                pagina = (value < 1) ? 1 : value;
+            }
+        }
         public int RecordsPorPagina
         {
             get
@@ -13,7 +22,8 @@
             }
             set
             {
-                recordsPorPagina = (value > cantidadMaximaRecordsPorPagina) ? cantidadMaximaRecordsPorPagina : value;
+                // Min value: 0; Max value: 50
+                recordsPorPagina = (value < 1) ? 0 : (value > cantidadMaximaRecordsPorPagina ? cantidadMaximaRecordsPorPagina : value);
             }
         }
 
