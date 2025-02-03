b81728df0dc6d90ce199050be21365cfcdfa3caf
Guillermo Flores <gflores.93@hotmail.com>
2025-02-02 22:09:20 -0600

    refactor: update initial map location

--- angular-peliculas/src/app/compartidos/componentes/mapa/mapa.component.ts
+++ angular-peliculas/src/app/compartidos/componentes/mapa/mapa.component.ts
@@ -9,49 +9,53 @@ import { Coordenada } from './coordenada';
   templateUrl: './mapa.component.html',
   styleUrl: './mapa.component.css'
 })
-export class MapaComponent implements OnInit{
+export class MapaComponent implements OnInit {
 
   ngOnInit(): void {
     this.capas = this.coordenadasIniciales.map(valor => {
       const marcador = marker([valor.latitud, valor.longitud], this.markerOptions);
+      this.initLat = valor.latitud;
+      this.initLong = valor.longitud;
       return marcador;
     });
+
+    this.options = {
+      layers: [
+        tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+          maxZoom: 18,
+          attribution: '...'
+        })
+      ],
+      zoom: 14,
+      center: latLng(this.initLat, this.initLong)
+    };
   }
 
   @Input() coordenadasIniciales: Coordenada[] = [];
-
   @Output() coordenadaSeleccionada = new EventEmitter<Coordenada>();
 
   markerOptions: MarkerOptions = {
     icon: icon({
       iconSize: [25, 41],
       iconAnchor: [13, 41],
-      iconUrl:  'assets/marker-icon.png',
+      iconUrl: 'assets/marker-icon.png',
       iconRetinaUrl: 'assets/marker-icon-2x.png',
       shadowUrl: 'assets/marker-shadow.png'
     })
   }
 
-  options = {
-    layers: [
-      tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        maxZoom: 18,
-        attribution: '...'
-      })
-    ],
-    zoom: 14,
-    center: latLng(49.59207151097279, 11.00551064129947) // test location
-  };
-
-
+  options = {};
   capas: Marker<any>[] = [];
-  
+  // Default test location
+  initLat: number = 49.59207151097279;
+  initLong: number = 11.00551064129947
+
   manejarClick(event: LeafletMouseEvent) {
     const latitud = event.latlng.lat;
     const longitud = event.latlng.lng;
 
     this.capas = [];
     this.capas.push(marker([latitud, longitud], this.markerOptions));
-    this.coordenadaSeleccionada.emit({latitud, longitud});
+    this.coordenadaSeleccionada.emit({ latitud, longitud });
   }
 }
