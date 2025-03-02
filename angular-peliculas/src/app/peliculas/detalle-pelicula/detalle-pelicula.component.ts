bceb87baf32005d6c3ae80753160ed7114425ea3
Guillermo Flores <gflores.93@hotmail.com>
2025-03-01 20:14:46 -0600

    refactor: reload pelicula info after changing rating

--- angular-peliculas/src/app/peliculas/detalle-pelicula/detalle-pelicula.component.ts
+++ angular-peliculas/src/app/peliculas/detalle-pelicula/detalle-pelicula.component.ts
@@ -21,19 +21,10 @@ import { RatingComponent } from "../../compartidos/componentes/rating/rating.com
 export class DetallePeliculaComponent implements OnInit {
 
   ngOnInit(): void {
-    this.peliculasService.obtenerPorId(this.id).subscribe(pelicula => {
-      // Due to deserialization, fechaLanzamiento is stored as a string
-      pelicula.fechaLanzamiento = new Date(pelicula.fechaLanzamiento); 
-      this.pelicula = pelicula;
-      this.trailerURL = this.generarURLYoutubeEmbed(pelicula.trailer);
-
-      this.coordenadas = pelicula.cines!.map(cine => {
-        return <Coordenada>{latitud: cine.latitud, longitud: cine.longitud, texto: cine.nombre};
-      });
-    });
+    this.loadData();
   }
 
-  @Input({transform: numberAttribute}) id!: number;
+  @Input({ transform: numberAttribute }) id!: number;
   pelicula!: PeliculaDTO;
   peliculasService = inject(PeliculasService);
   ratingService = inject(RatingService);
@@ -43,29 +34,43 @@ export class DetallePeliculaComponent implements OnInit {
   trailerURL!: SafeResourceUrl;
   coordenadas: Coordenada[] = [];
 
-generarURLYoutubeEmbed(url: string): SafeResourceUrl | string {
-  if (!url) {
-    return '';
+  generarURLYoutubeEmbed(url: string): SafeResourceUrl | string {
+    if (!url) {
+      return '';
+    }
+
+    var videoId = url.split('v=')[1];
+    var posicionAmpersand = videoId.indexOf('&');
+    if (posicionAmpersand !== -1) {
+      videoId = videoId.substring(0, posicionAmpersand);
+    }
+
+    return this.sanitizer.bypassSecurityTrustResourceUrl(`https://www.youtube.com/embed/${videoId}`)
   }
 
-  var videoId = url.split('v=')[1];
-  var posicionAmpersand = videoId.indexOf('&');
-  if (posicionAmpersand !== -1) {
-    videoId = videoId.substring(0, posicionAmpersand);
+  puntuar(puntuacion: number) {
+    if (!this.seguridadService.estaLogueado()) {
+      Swal.fire('Error', 'Debes loguearte para poder votar por una pelicula', 'error');
+      return;
+    }
+    
+    this.ratingService.puntuar(this.id, puntuacion).subscribe(() => {
+      Swal.fire('Exitoso', 'Su voto ha sido recibido', 'success');
+      this.loadData();
+    });
   }
 
-  return this.sanitizer.bypassSecurityTrustResourceUrl(`https://www.youtube.com/embed/${videoId}`)
-}
+  loadData() {
+    this.peliculasService.obtenerPorId(this.id).subscribe(pelicula => {
+      // Due to deserialization, fechaLanzamiento is stored as a string
+      pelicula.fechaLanzamiento = new Date(pelicula.fechaLanzamiento);
+      this.pelicula = pelicula;
+      this.trailerURL = this.generarURLYoutubeEmbed(pelicula.trailer);
 
-puntuar(puntuacion: number) {
-  if(!this.seguridadService.estaLogueado()) {
-    Swal.fire('Error', 'Debes loguearte para poder votar por una pelicula', 'error');
-    return;
+      this.coordenadas = pelicula.cines!.map(cine => {
+        return <Coordenada>{ latitud: cine.latitud, longitud: cine.longitud, texto: cine.nombre };
+      });
+    });
   }
-
-  this.ratingService.puntuar(this.id, puntuacion).subscribe(() => {
-    Swal.fire('Exitoso', 'Su voto ha sido recibido', 'success');
-  });
-}
 
 }
